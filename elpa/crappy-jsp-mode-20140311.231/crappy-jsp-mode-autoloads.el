db96bee65d19f75c4a65c616d5982f5351bcbe63
weiyongqing <weiyongqing123@sina.com>
2016-02-23 15:49:01 +0800

    inspiron update

---
+++ elpa/crappy-jsp-mode-20140311.231/crappy-jsp-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; crappy-jsp-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("crappy-jsp-mode-pkg.el" "crappy-jsp-mode.el")
+;;;;;;  (21886 26681 17375 706000))
+
+;;;***
+
+(provide 'crappy-jsp-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; crappy-jsp-mode-autoloads.el ends here
